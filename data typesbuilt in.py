24a8e93551eca33348eef1f211eaa75473b5e05f
aadhavan <gooppoitech@gmail.com>
2025-09-20 18:38:14 +0530

    Create data typesbuilt in.py

---
+++ data typesbuilt in.py
@@ -0,0 +1,129 @@
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+
+  x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+  x = 20
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+  x = 20.5
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+  x = 1j
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+x = ("apple", "banana", "cherry")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+  x = range(6)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+  x = frozenset({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+  x = {"apple", "banana", "cherry"}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = True
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+  x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x = 5
+print(type(x))
+
+  x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
