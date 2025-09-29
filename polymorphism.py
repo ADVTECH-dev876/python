8a878a1d3c893fc24f207ee22a33918440704508
aadhavan <gooppoitech@gmail.com>
2025-09-29 17:46:11 +0530

    Create polymorphism.py

---
+++ polymorphism.py
@@ -0,0 +1,28 @@
+Function Polymorphism
+An example of a Python function that can be used on different objects is the len() function.
+
+String
+For strings len() returns the number of characters:
+
+ExampleGet your own Python Server
+x = "Hello World!"
+
+print(len(x))
+Tuple
+For tuples len() returns the number of items in the tuple:
+
+Example
+mytuple = ("apple", "banana", "cherry")
+
+print(len(mytuple))
+Dictionary
+For dictionaries len() returns the number of key/value pairs in the dictionary:
+
+Example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(len(thisdict))
