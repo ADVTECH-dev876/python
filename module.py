f54e15c7b023d070d313b5904e33b28b0e8c3ab8
aadhavan <gooppoitech@gmail.com>
2025-09-29 17:49:17 +0530

    Create module.py

---
+++ module.py
@@ -0,0 +1,9 @@
+
+What is a Module?
+Consider a module to be the same as a code library.
+
+A file containing a set of functions you want to include in your application.
+import mymodule
+
+a = mymodule.person1["age"]
+print(a)
