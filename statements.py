9165a84a2f95a1fa9dfbf0e7acf88f6eba667dfb
aadhavan <gooppoitech@gmail.com>
2025-09-29 17:37:24 +0530

    Create statements.py

---
+++ statements.py
@@ -0,0 +1,45 @@
+Python Conditions and If statements
+Python supports the usual logical conditions from mathematics:
+
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+These conditions can be used in several ways, most commonly in "if statements" and loops.
+
+An "if statement" is written by using the if keyword.
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+//
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+
+
+//while
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+//loops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
