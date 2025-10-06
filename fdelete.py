571ec556e2e5b2125aa22fa2e4dca6c707713554
aadhavan <gooppoitech@gmail.com>
2025-10-06 10:30:23 +0530

    Create fdelete.py

---
+++ fdelete.py
@@ -0,0 +1,19 @@
+Delete a File
+To delete a file, you must import the OS module, and run its os.remove() function:
+
+ExampleGet your own Python Server
+Remove the file "demofile.txt":
+
+import os
+os.remove("demofile.txt")
+Check if File exist:
+To avoid getting an error, you might want to check if the file exists before you try to delete it:
+
+Example
+Check if file exists, then delete it:
+
+import os
+if os.path.exists("demofile.txt"):
+  os.remove("demofile.txt")
+else:
+  print("The file does not exist")
