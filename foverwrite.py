9d10bbcb9cfeedb689f1d5cd603bb7a62100b581
aadhavan <gooppoitech@gmail.com>
2025-10-06 10:29:48 +0530

    Create foverwrite.py

---
+++ foverwrite.py
@@ -0,0 +1,29 @@
+Python File Write
+Write to an Existing File
+To write to an existing file, you must add a parameter to the open() function:
+
+"a" - Append - will append to the end of the file
+
+"w" - Write - will overwrite any existing content
+
+
+Open the file "demofile.txt" and append content to the file:
+
+with open("demofile.txt", "a") as f:
+  f.write("Now the file has more content!")
+
+#open and read the file after the appending:
+with open("demofile.txt") as f:
+  print(f.read())
+Overwrite Existing Content
+To overwrite the existing content to the file, use the w parameter:
+
+Example
+Open the file "demofile.txt" and overwrite the content:
+
+with open("demofile.txt", "w") as f:
+  f.write("Woops! I have deleted the content!")
+
+#open and read the file after the overwriting:
+with open("demofile.txt") as f:
+  print(f.read())
